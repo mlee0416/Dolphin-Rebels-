fixed calculation error in total_bachelors column

gov_api.py
@@ -110,7 +110,9 @@ for column1, column2 in columns_to_aggregate:
 df['total_pop'] = df[['femaletotal','maletotal']].sum(axis=1)
 
 # get total bachelors degree holders
-df['total_bachelors'] = df['total_pop'] / df['percent_bachelors']
+df['total_bachelors'] = df['total_pop'] * (df['percent_bachelors'] / 100)
+
+df['bachelors_percap'] = df['total_bachelors'] / df['total_pop']
 
 df.head()     
 